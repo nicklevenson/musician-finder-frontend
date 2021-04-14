72628f1fd165ec66b86d6923cb941397ef111b45
Nicholas Levenson <nicklevenson@gmail.com>
2021-04-14 11:06:23 -0700

    initial grid setup

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import UserCard from '../components/UserCard'
-
+import {Grid} from 'semantic-ui-react'
 class HomeContainer extends React.Component {
   render(){
     return(
       <>
       <h1>Home</h1>
-      {this.props.currentUser.username ? <UserCard/> : null}
+        <Grid celled>
+        <Grid.Row>
+          <Grid.Column width={3}>
+            {this.props.currentUser.username ? <UserCard/> : null}
+          </Grid.Column>
+          <Grid.Column width={10}>
+            {this.props.currentUser.username ? <UserCard/> : null}
+          </Grid.Column>
+          <Grid.Column width={3}>
+            {this.props.currentUser.username ? <UserCard/> : null}
+          </Grid.Column>
+        </Grid.Row>
+        </Grid>
       </>
     )
   }
