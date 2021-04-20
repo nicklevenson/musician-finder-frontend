962138029f5132ec5d766c054132a0abda5b790e
Nicholas Levenson <nicklevenson@gmail.com>
2021-04-20 16:49:27 -0700

    notification setup

--- src/containers/NotificationsContainer.js
+++ src/containers/NotificationsContainer.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
+import {fetchUser} from '../actions/useractions'
+import {Notification} from '../components/Notification.js'
+import {Feed} from 'semantic-ui-react'
 class NotificationsContainer extends React.Component {
+  componentDidMount = () => {
+    this.props.fetchUser()
+  }
   render(){
     if (sessionStorage.jwt){
       return(
+        <>
         <h1>Notifications</h1>
+        <Feed>
+          {this.props.currentUser.notifications.map(n => {
+            return <Notification notification={n}/>
+          })}
+        </Feed>
+        </>
       )
     }
     else{
@@ -15,4 +29,16 @@ class NotificationsContainer extends React.Component {
   }
 }
 
-export default NotificationsContainer
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUser: () => dispatch(fetchUser())
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.currentUser.currentUser
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NotificationsContainer)
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,6 +1,7 @@
 export default function userReducer(state={
   currentUser: {
-    outgoing_pending_requests: []
+    outgoing_pending_requests: [],
+    notifications: []
   },
   connectedUsers: [],
   recommendedUsers: [],
