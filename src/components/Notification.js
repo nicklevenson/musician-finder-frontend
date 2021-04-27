f9de1f4c5098c9d9eb5baa57c2f4fa7521a52bf2
Nicholas Levenson <nicklevenson@gmail.com>
2021-04-26 18:53:21 -0700

    notification fix

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,10 +1,14 @@
+
+import React from 'react'
 import {Feed, Icon} from 'semantic-ui-react'
 import {connect} from 'react-redux'
 import {fetchUser} from '../actions/useractions'
 
-const Notification = (props) => {
-
-  function makeRead(){
+class Notification extends React.Component {
+  state = {
+    background: this.props.notification.read ? "white" : "lightgrey"
+  }
+  makeRead = (e) => {
     let configObj = {
       method: 'POST',
       headers: {
@@ -12,38 +16,42 @@ const Notification = (props) => {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ids: [props.notification.id]})
+      body: JSON.stringify({ids: [this.props.notification.id]})
     }
     const userId = sessionStorage.userId
     fetch(`${process.env.REACT_APP_BACKEND_URL}/notifications/make_read`,configObj)
-    .then(resp => {
-      props.fetchUser()
-    })
+    this.setState({background: "white"})
+    this.props.fetchUser()
   }
 
-  return(
-    <a href={props.notification.involved_user_id ? "/users/" + props.notification.involved_user_id : null}>
-    <Feed.Event style={props.notification.read ? null : {background: "lightgrey"}} onClick={makeRead} className="notification">
-      {/* <Feed.Label>
-        <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
-      </Feed.Label> */}
-      <Feed.Content>
-        <Feed.Summary style={{textAlign:"center"}}>
-          {/* <Feed.User>Elliot Fu</Feed.User> added you as a friend */}
-          {props.notification.involved_user_id ?
-            <>
-              {props.notification.involved_username + " "}
-              {props.notification.content}
-            </>
-            :
-           props.notification.content
-          }
-          <Feed.Date>{props.notification.created_at.split("-").splice(0,2).join("-")}</Feed.Date>
-        </Feed.Summary>
-      </Feed.Content>
-    </Feed.Event>
-    </a>
-  )
+  render(){
+    return(
+  
+      <Feed.Event style={this.props.notification.read ? null : {background: this.state.background}} onClick={this.props.notification.read ? null : e => this.makeRead(e)} className="notification">
+        <a href={this.props.notification.involved_user_id ? "/users/" + this.props.notification.involved_user_id : null}>
+        {/* <Feed.Label>
+          <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+        </Feed.Label> */}
+        <Feed.Content>
+          <Feed.Summary style={{textAlign:"center"}}>
+            {/* <Feed.User>Elliot Fu</Feed.User> added you as a friend */}
+            {this.props.notification.involved_user_id ?
+              <>
+                {this.props.notification.involved_username + " "}
+                {this.props.notification.content}
+              </>
+              :
+             this.props.notification.content
+            }
+            <Feed.Date>{this.props.notification.created_at.split("-").splice(0,2).join("-")}</Feed.Date>
+          </Feed.Summary>
+        </Feed.Content>
+        </a>
+      </Feed.Event>
+   
+    )
+  }
+  
 }
 
 const mapDispatchToProps = (dispatch) => {
