d980d8de056a753fede37a7a2fd2413daedb4d07
Ryan Grunest <ryan.grunest@realpage.com>
2021-04-29 20:48:30 -0500

    styling for main nav, add scss

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import './App.css';
-import {Route, Switch} from 'react-router-dom'
-import Nav from './components/Nav'
-import HomeContainer from './containers/HomeContainer'
-import ConnectionsContainer from './containers/ConnectionsContainer'
-import MessagingContainer from './containers/MessagingContainer'
-import NotificationsContainer from './containers/NotificationsContainer'
-import ProfileContainer from './containers/ProfileContainer'
-import UserShowContainer from './containers/UserShowContainer'
-import Logout from './components/Logout'
+import React from "react";
+import { connect } from "react-redux";
+import "./App.css";
+import "./style/index.scss";
+import { Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import HomeContainer from "./containers/HomeContainer";
+import ConnectionsContainer from "./containers/ConnectionsContainer";
+import MessagingContainer from "./containers/MessagingContainer";
+import NotificationsContainer from "./containers/NotificationsContainer";
+import ProfileContainer from "./containers/ProfileContainer";
+import UserShowContainer from "./containers/UserShowContainer";
+import Logout from "./components/Logout";
 
-import {fetchUser, fetchAllUsers} from './actions/useractions'
-import LoginContainer from './containers/LoginContainer';
+import { fetchUser, fetchAllUsers } from "./actions/useractions";
+import LoginContainer from "./containers/LoginContainer";
 class App extends React.Component {
   componentDidMount() {
-    this.props.fetchAllUsers()
+    this.props.fetchAllUsers();
     if (sessionStorage.userId) {
-      this.props.fetchUser()
+      this.props.fetchUser();
     }
-  
   }
-  render(){
+  render() {
     return (
       <div className="App">
-        <Nav currentUser={this.props.currentUser}/>
-        <div style={{paddingTop: "100px"}}>
+        <Nav currentUser={this.props.currentUser} />
+        <div style={{ paddingTop: "100px" }}>
           <Switch>
             <Route exact path="/home" component={HomeContainer}></Route>
-            <Route exact path="/connections" component={ConnectionsContainer}></Route>
-            <Route exact path="/messaging" component={MessagingContainer}></Route>
-            <Route exact path="/notifications" component={NotificationsContainer}></Route>
+            <Route
+              exact
+              path="/connections"
+              component={ConnectionsContainer}
+            ></Route>
+            <Route
+              exact
+              path="/messaging"
+              component={MessagingContainer}
+            ></Route>
+            <Route
+              exact
+              path="/notifications"
+              component={NotificationsContainer}
+            ></Route>
             <Route exact path="/profile" component={ProfileContainer}></Route>
-            <Route exact path="/users/:id" component={UserShowContainer}></Route>
+            <Route
+              exact
+              path="/users/:id"
+              component={UserShowContainer}
+            ></Route>
             <Route exact path="/login" component={LoginContainer}></Route>
             <Route exact path="/logout" component={Logout}></Route>
           </Switch>
@@ -40,20 +56,19 @@ class App extends React.Component {
       </div>
     );
   }
-  
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchUser: () => dispatch(fetchUser()),
-    fetchAllUsers: () => dispatch(fetchAllUsers())
-  }
-}
+    fetchAllUsers: () => dispatch(fetchAllUsers()),
+  };
+};
 
 const mapStateToProps = (state) => {
   return {
-    currentUser: state.currentUser.currentUser
-  }
-}
+    currentUser: state.currentUser.currentUser,
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,98 +1,133 @@
-import React from 'react'
-import {Menu, Icon} from 'semantic-ui-react'
-import {NavLink } from 'react-router-dom'
-import ProfileImage from './ProfileImage'
-import NotificationIcon from './NotificationIcon'
-class Nav extends React.Component {
-  state = {}
+import { Component } from "react";
+import { Menu, Icon } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
+import ProfileImage from "./ProfileImage";
+import NotificationIcon from "./NotificationIcon";
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false,
+    };
+  }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-  render(){
-    const { activeItem } = this.state
+  handleHamburgerClick = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    if (!e.target.classList.contains("hamburger")) {
+      e.target = e.target.parentNode;
+    }
+    console.log(e.target);
+    e.target.classList.toggle("is-active");
+    this.setState({ isActive: !this.state.isActive });
+  };
+
+  render() {
+    const { activeItem } = this.state;
 
     return (
-      
-      <Menu fixed="top" secondary pointing borderless style={{paddingLeft: "10vw", paddingRight: "10vw", paddingTop:"1vh", paddingBottom:"1vh", backgroundColor: "white"}}>
-        <Menu.Item 
-          header
-          href="/home"
-          as={"a"}
-        >
-          <div>
-            <div><Icon name="music" size="large" fitted/></div>
-            Matchup Music
+      <>
+        <div className="main-navigation">
+          <div className="main-logo-container">
+            <Menu.Item header href="/home" as={"a"}>
+              <div>
+                <div>
+                  <Icon name="music" size="large" fitted />
+                </div>
+                Matchup Music
+              </div>
+            </Menu.Item>
           </div>
-        </Menu.Item>
-    
-          <Menu.Item
-            position="right"
-            name='home'
-            active={activeItem === 'home'}
-            onClick={this.handleItemClick}
-            as={NavLink}
-            to="/home"
-          >  
-            <div>
-              <div><Icon name="home" size="large" fitted/></div>
-              Home
-            </div>
-          </Menu.Item>
-       
-          <Menu.Item
-            name='connections'
-            active={activeItem === 'connections'}
-            onClick={this.handleItemClick}
-            as={NavLink}
-            to="/connections"
+
+          <div
+            className="hamburger"
+            onClick={(e) => this.handleHamburgerClick(e)}
           >
-            <div>
-              <div><Icon name="users" size="large" fitted/></div>
-              Connections
-            </div>
-          </Menu.Item>
-   
-
-        <Menu.Item
-          name='messaging'
-          active={activeItem === 'messaging'}
-          onClick={this.handleItemClick}
-          as={NavLink}
-          to="/messaging"
-        >
-           <div>
-              <div><Icon name="chat" size="large" fitted/></div>
-              Messaging
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
-        </Menu.Item>
+        </div>
+        {this.state.isActive ? (
+          <div className="main-nav-dropdown-menu">
+            <Menu.Item
+              name="home"
+              active={activeItem === "home"}
+              onClick={this.handleItemClick}
+              as={NavLink}
+              to="/home"
+            >
+              <div>
+                <div>
+                  <Icon name="home" size="large" fitted />
+                </div>
+                Home
+              </div>
+            </Menu.Item>
 
-        <Menu.Item
-          name='notifications'
-          active={activeItem === 'notifications'}
-          onClick={this.handleItemClick}
-          as={NavLink}
-          to="/notifications"
-        >
-          <div>
-            <NotificationIcon/>
-            Notifications
-          </div>
-        </Menu.Item>
+            <Menu.Item
+              name="connections"
+              active={activeItem === "connections"}
+              onClick={this.handleItemClick}
+              as={NavLink}
+              to="/connections"
+            >
+              <div>
+                <div>
+                  <Icon name="users" size="large" fitted />
+                </div>
+                Connections
+              </div>
+            </Menu.Item>
 
-        <Menu.Item
-          name='Profile'
-          active={activeItem === 'Profile'}
-          onClick={this.handleItemClick}
-          as={NavLink}
-          to="/profile"
-        >
-          <div>
-            <ProfileImage/>
-            Profile
+            <Menu.Item
+              name="messaging"
+              active={activeItem === "messaging"}
+              onClick={this.handleItemClick}
+              as={NavLink}
+              to="/messaging"
+            >
+              <div>
+                <div>
+                  <Icon name="chat" size="large" fitted />
+                </div>
+                Messaging
+              </div>
+            </Menu.Item>
+
+            <Menu.Item
+              name="notifications"
+              active={activeItem === "notifications"}
+              onClick={this.handleItemClick}
+              as={NavLink}
+              to="/notifications"
+            >
+              <div>
+                <NotificationIcon />
+                Notifications
+              </div>
+            </Menu.Item>
+
+            <Menu.Item
+              name="Profile"
+              active={activeItem === "Profile"}
+              onClick={this.handleItemClick}
+              as={NavLink}
+              to="/profile"
+            >
+              <div>
+                <ProfileImage />
+                Profile
+              </div>
+            </Menu.Item>
           </div>
-        </Menu.Item>
-      </Menu>
-    )
+        ) : (
+          ""
+        )}
+      </>
+    );
   }
 }
 
-export default Nav
+export default Nav;
