beea5b75ec8d5141c4de61a5bf6226d3e7282479
Ryan Grunest <ryan.grunest@realpage.com>
2021-04-30 10:53:12 -0500

    refactor nav buttons, add functionality to change state of nav on click and change url

--- src/components/Users/ProfileImage.js
+++ src/components/Users/ProfileImage.js
@@ -1,23 +1,25 @@
-import {connect} from 'react-redux'
-import {Image} from 'semantic-ui-react'
+import { connect } from "react-redux";
+import { Image } from "semantic-ui-react";
 const ProfileImage = (props) => {
-  
-  return(
-    <div>
+  return (
+    <>
       <Image
-        style={{width:"25px"}}
         circular
-        src={sessionStorage.jwt ? props.currentUser.photo || props.currentUser.providerImage : "https://icon-library.net//images/no-user-image-icon/no-user-image-icon-27.jpg"}
+        src={
+          sessionStorage.jwt
+            ? props.currentUser.photo || props.currentUser.providerImage
+            : "https://icon-library.net//images/no-user-image-icon/no-user-image-icon-27.jpg"
+        }
         centered
       />
-    </div>
-  )
-}
+    </>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
-    currentUser: state.currentUser.currentUser
-  }
-}
+    currentUser: state.currentUser.currentUser,
+  };
+};
 
-export default connect(mapStateToProps)(ProfileImage)
+export default connect(mapStateToProps)(ProfileImage);
