f61b6261d8bbddbb23046e3721d5ac7648d8a3d6
Nicholas Levenson <nicklevenson@gmail.com>
2021-05-03 18:21:22 -0700

    make read

--- src/components/Messages/Chatroom.js
+++ src/components/Messages/Chatroom.js
@@ -2,9 +2,17 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Image} from 'semantic-ui-react'
 import {Message} from './Message.js'
+import { makeMessageRead } from '../../actions/useractions.js'
 
 class Chatroom extends React.Component {
-  
+  componentDidMount(){
+    const chatroom = this.props.chatrooms.find(cr => cr.id === this.props.chatroomId)
+    const lastMessage = chatroom?.messages[chatroom.messages.length - 1]
+    if (lastMessage?.read === false && lastMessage.user_id !== this.props.currentUser.id){
+      console.log("making message read")
+      this.props.makeMessageRead(lastMessage.id)
+    }
+  }
   render(){
     const chatroom = this.props.chatrooms.find(cr => cr.id === this.props.chatroomId)
     const otherUser = chatroom.users.find(u => u.id !== this.props.currentUser.id)
@@ -32,6 +40,13 @@ class Chatroom extends React.Component {
     )
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    makeMessageRead: (message_id) => dispatch(makeMessageRead(message_id))
+  };
+};
+
 const mapStateToProps = (state) => {
   return {
     currentUser: state.currentUser.currentUser,
@@ -40,4 +55,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Chatroom)
+export default connect(mapStateToProps, mapDispatchToProps)(Chatroom)
--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,3 +1,4 @@
+
 import {Image} from 'semantic-ui-react'
 export const Message = (props) => {
   return(
