bd6e08e3113d7b84b775ed059c06f9c22e1c0b21
Nicholas Levenson <nicklevenson@gmail.com>
2021-05-05 18:50:28 -0700

    got swiping working

--- src/containers/RecommendedUsers.js
+++ src/containers/RecommendedUsers.js
@@ -8,10 +8,15 @@ import Filter from '../components/Misc/Filter'
 class RecommendedUsers extends React.Component {
 
   state = {
-    activeIndex: 0
+    activeIndex: 0,
+    margin: 0
   }
   componentDidMount() {
     this.props.fetchUserRecs()
+  }
+
+  componentDidUpdate(){
+    this.setMargin()
   }
 
   cardChange = (e) => {
@@ -22,18 +27,32 @@ class RecommendedUsers extends React.Component {
         activeIndex: state.activeIndex + 1
       }))
     }
-   
+    if (this.state.activeIndex === this.props.recommendedUsers.length - 1){
+      this.setState({margin: 0})
+    }else{
+      this.setState((state) => ({
+        margin: state.margin + window.innerWidth
+      }))
+    }
+  }
+
+
+
+  setMargin = () => {
+    const container = document.querySelector('.cards-container')
+    container.scrollLeft = this.state.margin
   }
   render() {
     return(
       <div className="recommended-users">
           <Filter/>
-          {this.props.recommendedUsers?.map((u, index) =>
-            <div className={this.state.activeIndex === index ? "active card" : "inactive card"}>
-              <PreviewUserCard user={u.user} similar_tags={u.similar_tags} key={index} cardChange={this.cardChange}/>
-            </div>
-          )} 
-    
+          <div className="cards-container">
+            {this.props.recommendedUsers?.map((u, index) =>
+              <div className={this.state.activeIndex === index ? "active card" : "inactive card"}>
+                <PreviewUserCard user={u.user} similar_tags={u.similar_tags} key={index} cardChange={this.cardChange}/>
+              </div>
+            )} 
+          </div>
       </div>
     )
   }
