70797dd0509db5d01de006de32a9b6fb49415c1e
Nicholas Levenson <nicklevenson@gmail.com>
2021-05-19 11:06:04 -0700

    instrument options are taken from our list and can be clicked

--- src/components/Misc/InstrumentOptions.js
+++ src/components/Misc/InstrumentOptions.js
@@ -1,37 +1,58 @@
-import React from 'react'
-
+import React from "react";
+import { connect } from "react-redux";
 
 class InstrumentOptions extends React.Component {
   state = {
-    inputQuery: ""
-  }
+    inputQuery: "",
+    results: [],
+  };
 
   setInputQuery = (e) => {
-    this.setState({inputQuery: e.target.value})
-  }
+    this.setState({ inputQuery: e.target.value });
+    const filteredInstruments = this.props.instruments
+      .filter((instrument) => {
+        return instrument.toLowerCase().includes(e.target.value.toLowerCase());
+      })
+      .splice(0, 9);
+    this.setState({ results: filteredInstruments });
+  };
 
   handleClick = (e) => {
-    e.preventDefault()
-    this.props.setInstruments(this.state.inputQuery)
-  }
+    e.preventDefault();
+    const selection = e.target.innerText;
+    this.props.setInstruments(selection);
+  };
 
-  
-  render(){
-    return(
+  render() {
+    return (
       <div className="instruments-filter">
         <label htmlFor="instruments filter">By Instruments</label>
-        <br/>
-        <input 
-          name="instruments filter" 
-          className="filter-input" 
-          placeholder="Search for Instrument" 
+        <br />
+        <input
+          name="instruments filter"
+          className="filter-input"
+          placeholder="Search for Instrument"
           value={this.state.inputQuery}
-          onInput={e => this.setInputQuery(e)}
+          onInput={(e) => this.setInputQuery(e)}
         />
-        <button onClick={e => this.handleClick(e)}>Add</button>
+
+        <div
+          className="instruments-results"
+          onClick={(e) => this.handleClick(e)}
+        >
+          {this.state.results.map((result) => {
+            return <div>{result}</div>;
+          })}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default InstrumentOptions
+const mapStateToProps = (state) => {
+  return {
+    instruments: state.lists.instruments,
+  };
+};
+
+export default connect(mapStateToProps)(InstrumentOptions);
