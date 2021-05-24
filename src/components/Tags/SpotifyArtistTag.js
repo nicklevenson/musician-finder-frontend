607182fe75c5d71f4acd5e24b35f3057e3d0ecc9
Nicholas Levenson <nicklevenson@gmail.com>
2021-05-24 12:06:33 -0700

    char count for artist tag

--- src/components/Tags/SpotifyArtistTag.js
+++ src/components/Tags/SpotifyArtistTag.js
@@ -3,8 +3,8 @@ const SpotifyArtistTag = (props) => {
     <div className="generic-tag spotify-artist-tag">
       <img className="artist-image" src={props.tag.image_url}></img>
       <div className="artist-name">
-        {props.tag.name.substr(0, 19)}
-        {props.tag.name.length > 20 ? "..." : null}
+        {props.tag.name.substr(0, 35)}
+        {props.tag.name.length > 36 ? "..." : null}
       </div>
     </div>
   );
