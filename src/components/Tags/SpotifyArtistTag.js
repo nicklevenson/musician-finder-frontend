0f9cecc7c522564bb38ee8898b1d5044a0842729
Nicholas Levenson <nicklevenson@gmail.com>
2021-05-23 17:04:31 -0700

    spotify tag has name

--- src/components/Tags/SpotifyArtistTag.js
+++ src/components/Tags/SpotifyArtistTag.js
@@ -2,7 +2,10 @@ const SpotifyArtistTag = (props) => {
   return (
     <div className="generic-tag spotify-artist-tag">
       <img className="artist-image" src={props.tag.image_url}></img>
-      {/* <div className="artist-name">{props.tag.name}</div> */}
+      <div className="artist-name">
+        {props.tag.name.substr(0, 19)}
+        {props.tag.name.length > 20 ? "..." : null}
+      </div>
     </div>
   );
 };
