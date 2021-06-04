c8d82ce8833a7e6750f2361977601cbe9f340e79
Nicholas Levenson <nicklevenson@gmail.com>
2021-06-04 11:52:49 -0700

    displays genres and can select genres

--- src/components/Users/UserEditorModal/EditGenresForm.js
+++ src/components/Users/UserEditorModal/EditGenresForm.js
@@ -1,24 +1,35 @@
 import { Component } from "react";
-
+import GenreOptions from "../../Misc/GenreOptions";
+import GenericTag from "../../Tags/GenericTag";
 class EditGenresForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {
+    genres: this.props.user.genres.map((genre) => genre.name),
+  };
 
-  componentDidMount() {}
+  setGenres = (genre) => {
+    console.log(genre);
+    this.setState((state) => ({
+      genres: state.genres.concat(genre),
+    }));
+  };
 
   render() {
     return (
-      <form>
-        <div className="form-group">
-          <label htmlFor="genres">Add Genre</label>
-          <input type="text" placeholder="genre" />
-        </div>
-        <button className="save-btn" type="button">
-          Update Genres
-        </button>
-      </form>
+      <>
+        <form>
+          <div className="form-group">
+            <GenreOptions setGenres={this.setGenres} />
+          </div>
+          <div>
+            {this.state.genres.map((genre) => (
+              <GenericTag tag={genre} />
+            ))}
+          </div>
+          <button className="save-btn" type="button">
+            Update Genres
+          </button>
+        </form>
+      </>
     );
   }
 }
