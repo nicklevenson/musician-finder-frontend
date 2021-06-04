f595608be9413c7b59bfde52ac2931605d5dc300
Nick Levenson <68400585+nicklevenson@users.noreply.github.com>
2021-06-03 20:10:39 -0700

    Messages mobile friendly (#24)
    make mobile messages friendly

--- src/components/Messages/Chatroom.js
+++ src/components/Messages/Chatroom.js
@@ -37,37 +37,49 @@ class Chatroom extends React.Component {
       (u) => u.id !== this.props.currentUser.id
     );
     return (
-      <div className="chatroom">
-        <div className="chatroom-header">
-          <Image
-            size="tiny"
-            src={otherUser.photo || otherUser.providerImage}
-            circular
-            inline
-          />
-          <b> {otherUser.username}</b>
-        </div>
-
-        <div className="messages-container">
-          {chatroom.messages.length < 1 ? <h5>Start a conversation</h5> : null}
-
-          {chatroom.messages.reverse().map((message) => {
-            return (
-              <Message
-                message={message}
-                key={message.id}
-                currentUserMessage={
-                  this.props.currentUser.id === message.user_id ? true : null
-                }
+      <>
+        <div className="chatroom">
+          <div className="chatroom-header">
+            <div
+              className="back-button"
+              onClick={(e) => this.props.handleBackToPreview()}
+            >
+              <i>Back</i>
+            </div>
+            <div className="chatting-with">
+              <Image
+                size="mini"
+                src={otherUser.photo || otherUser.providerImage}
+                circular
+                inline
               />
-            );
-          })}
-        </div>
+              <b> {otherUser.username}</b>
+            </div>
+          </div>
 
-        <div className="new-message-container">
-          <NewMessage chatroomId={chatroom.id} />
+          <div className="messages-container">
+            {chatroom.messages.length < 1 ? (
+              <h5>Start a conversation</h5>
+            ) : null}
+
+            {chatroom.messages.reverse().map((message) => {
+              return (
+                <Message
+                  message={message}
+                  key={message.id}
+                  currentUserMessage={
+                    this.props.currentUser.id === message.user_id ? true : null
+                  }
+                />
+              );
+            })}
+          </div>
+
+          <div className="new-message-container">
+            <NewMessage chatroomId={chatroom.id} />
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
