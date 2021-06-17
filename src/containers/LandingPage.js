e0069b78f4159eb612ef8bca215f49ef39bb01c5
Nicholas Levenson <nicklevenson@gmail.com>
2021-06-16 17:15:08 -0700

    loglines

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -6,6 +6,21 @@ class LandingPage extends React.Component {
     return (
       <div className="landing-page-container">
         <h1>Welcome to Peanut Butter and Jam!</h1>
+        <div className="brochure">
+          <div>
+            A place for musicians to connect based on similar interests,
+            location, and other preferences
+          </div>
+          <br />
+          <div>
+            Find band chemistry quick by swiping through musicians sorted by how
+            much they have in common with you
+          </div>
+          <br />
+          <div>Start a conversation with your connected musicians</div>
+          <br />
+          <div>Build a network of like-minded musicians</div>
+        </div>
         <LoginCard />
       </div>
     );
