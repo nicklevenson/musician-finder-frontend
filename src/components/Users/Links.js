9243bb93bee98af6c98e8e584a36cf73468e8da6
Nicholas Levenson <nicklevenson@gmail.com>
2021-06-22 12:42:23 -0700

    links will work

--- src/components/Users/Links.js
+++ src/components/Users/Links.js
@@ -1,13 +1,45 @@
 import { Icon } from "semantic-ui-react";
 export const Links = (props) => {
+  const {
+    soundcloud_link,
+    bandcamp_link,
+    spotify_link,
+    apple_music_link,
+    youtube_link,
+    instagram_link,
+  } = props.user;
   return (
     <div className="links">
-      {props.user.soundcloud_link ? <Icon name="soundcloud" /> : null}
-      {props.user.bandcamp_link ? <Icon name="bandcamp" /> : null}
-      {props.user.spotify_link ? <Icon name="spotify" /> : null}
-      {props.user.apple_music_link ? <Icon name="apple" /> : null}
-      {props.user.youtube_link ? <Icon name="youtube" /> : null}
-      {props.user.instagram_link ? <Icon name="instagram" /> : null}
+      {soundcloud_link ? (
+        <a href={soundcloud_link} target="blank">
+          <Icon name="soundcloud" />
+        </a>
+      ) : null}
+      {bandcamp_link ? (
+        <a href={bandcamp_link} target="blank">
+          <Icon name="bandcamp" />
+        </a>
+      ) : null}
+      {spotify_link ? (
+        <a href={spotify_link} target="blank">
+          <Icon name="spotify" />{" "}
+        </a>
+      ) : null}
+      {apple_music_link ? (
+        <a href={apple_music_link} target="blank">
+          <Icon name="apple" />
+        </a>
+      ) : null}
+      {youtube_link ? (
+        <a href={youtube_link} target="blank">
+          <Icon name="youtube" />{" "}
+        </a>
+      ) : null}
+      {instagram_link ? (
+        <a href={instagram_link} target="blank">
+          <Icon name="instagram" />{" "}
+        </a>
+      ) : null}
     </div>
   );
 };
