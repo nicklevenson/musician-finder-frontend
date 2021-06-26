b0e1a99f657c1b5571e3acaf4944fdf946a4d493
Nicholas Levenson <nicklevenson@gmail.com>
2021-06-26 13:32:59 -0700

    welcome

--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -5,22 +5,30 @@ class Welcome extends React.Component {
     return (
       <div className="landing-page-container">
         <div className="landing-page">
-          <h1>Thanks for signing up for Peanut Butter and Jam!</h1>
-          <span>
-            We hope you can use PBJ to find musicians that you vibe with.
-          </span>
-          <b>
-            The best way to get matched with the right people is to add interest
-            tags to your profile. If you logged in with Spotify it should have
-            created a few for you already.
-          </b>
-          <span>
-            Add the instruments you play and the genres you affiliate with.
-          </span>
-          <span>
-            Adding your location is how you can search for musicians based on
-            their proximity to you.
-          </span>
+          <div className="brochure">
+            <h1>Thanks for signing up for Peanut Butter and Jam!</h1>
+            <br />
+            <div>
+              We hope you can use PBJ to find musicians that you vibe with.
+            </div>
+            <br />
+            <div>
+              <b>
+                The best way to get matched with the right people is to add
+                interest tags to your profile. If you logged in with Spotify it
+                should have created a few for you already.
+              </b>
+            </div>
+            <br />
+            <div>
+              Add the instruments you play and the genres you affiliate with.
+            </div>
+            <br />
+            <div>
+              Adding your location is how you can search for musicians based on
+              their proximity to you.
+            </div>
+          </div>
         </div>
       </div>
     );
